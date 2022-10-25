09d786e6ddb1a9434f2a3f0402040cad8c960fe7
learning-acc-cococry <githublearningluca@gmail.com>
2022-10-25 15:02:04 +0200

    added lib directory to YAML

--- engine/vendor/yaml-cpp/build.bat
+++ engine/vendor/yaml-cpp/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set sourceFiles=src/*.cpp src/contrib/*.cpp -Iinclude
-set compilerFlags=-fuse-ld=llvm-lib -std=c++17 -o lib/yaml-cpp.lib -DYAML_CPP_STATIC_DEFINE -DYAML_CPP_API
+set compilerFlags=-fuse-ld=llvm-lib -std=c++17 -o lib/yaml-cpp.lib -DYAML_CPP_STATIC_DEFINE 
 
 clang++ %sourceFiles% %compilerFlags%
